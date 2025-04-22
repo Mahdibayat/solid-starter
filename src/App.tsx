c25b041e7970976115ba67a59a483940ee02db11
bayat <bayat@safarbooking.com>
2025-04-22 17:05:20 +0330

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,14 @@
-import { Route, Router } from '@solidjs/router';
-import MainLayout from './components/layout/mainLayout';
-import DashboardPage from './components/pages/dashboard';
-import PageTwo from './components/pages/pageTwo';
+import { Route, Router } from "@solidjs/router";
+import MainLayout from "./components/layout/mainLayout";
+import DashboardPage from "./components/pages/dashboard";
+import PageTwo from "./components/pages/pageTwo";
 
 function App() {
   return (
     <Router>
-      <Route
-        path={'/'}
-        component={MainLayout}
-      >
-        <Route
-          path={'/'}
-          component={DashboardPage}
-        />
-        <Route
-          path={'/two'}
-          component={PageTwo}
-        />
+      <Route path={"/"} component={MainLayout}>
+        <Route path={"/"} component={DashboardPage} />
+        <Route path={"/two"} component={PageTwo} />
       </Route>
     </Router>
   );
